90fef4c59af615eeca7149d0a2a563af53724df5
mtgto <hogerappa@gmail.com>
2024-11-01 21:34:22 +0900

    Implement

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-rust-skkdict-sort
+skkdic-sort.rs
 ====
+Rustで[skkdictools](https://github.com/skk-dev/skktools)のskkdic-sort.cを書き直したものです。
+
+- UTF-8のみ対応
+- 先頭のコメントは残すことができます
+
+## 使い方
+
+TBD
+
+## ライセンス
+
+GPLv2
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use std::io;
+
+// NOTE: This program does not trim UTF-8 BOM.
+// Use: `sed '1s/^\xEF\xBB\xBF//' <input>` to remove BOM.
+
 fn main() {
-    println!("Hello, world!");
+    let mut okuri_ari_entries: Vec<(String, String)> = vec![];
+    let mut okuri_nasi_entries : Vec<(String, String)> = vec![];
+    let mut found_body = false;
+
+    for line_result in io::stdin().lines() {
+        let line = line_result.unwrap();
+        if line.starts_with(';') {
+            if found_body {
+                continue;
+            }
+            if line != ";; okuri-ari entries." && line != ";; okuri-nasi entries." {
+                println!("{}", line);
+            }
+        } else {
+            found_body = true;
+            let mut parts = line.splitn(2, " ");
+            match (parts.next(), parts.next()) {
+                (Some(yomi), Some(left)) => {
+                    match (yomi.chars().nth_back(0), yomi.chars().nth_back(1)) {
+                        (Some(a), Some(b)) if 'a' <= a && a <= 'z' && (b < 'a' || 'z' < b) => {
+                            okuri_ari_entries.push((yomi.to_string(), left.to_string()));
+                        }
+                        _ => {
+                            okuri_nasi_entries.push((yomi.to_string(), left.to_string()));
+                        }
+                    }
+                }
+                _ => {
+                    // no space
+                }
+            }
+        }
+    }
+    println!(";; okuri-ari entries.");
+    okuri_ari_entries.sort();
+    for (yomi, left) in okuri_ari_entries.iter().rev() {
+        println!("{} {}", yomi, left);
+    }
+    println!(";; okuri-nasi entries.");
+    okuri_nasi_entries.sort();
+    for (yomi, left) in okuri_nasi_entries {
+        println!("{} {}", yomi, left);
+    }
 }
